d4854c3c8942336b4c6c60835078fd561b1a02ef
wuliang <wy19941007>
2020-04-10 23:19:29 +0800

    哈哈哈哈

--- app/src/main/java/com/skyvn/ten/util/ToastManager.java
+++ app/src/main/java/com/skyvn/ten/util/ToastManager.java
@@ -1,6 +1,8 @@
 package com.skyvn.ten.util;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,28 +14,35 @@ import com.skyvn.ten.R;
 public class ToastManager {
 
     public static void showShort(String message) {
-//        Toast sToast = new Toast(AppManager.getAppManager().curremtActivity());
-//        View v = Toast.makeText(AppManager.getAppManager().curremtActivity(), "", Toast.LENGTH_SHORT).getView();
-//        sToast.setView(v);
-//        sToast.setText(message);
-//        sToast.setDuration(Toast.LENGTH_SHORT);
-//        sToast.show();
 
-        Context context = AppManager.getAppManager().curremtActivity();
-        //使用布局加载器，将编写的toast_layout布局加载进来
-        View view = LayoutInflater.from(context).inflate(R.layout.toast_view, null);
-        //获取TextView
-        TextView title = view.findViewById(R.id.toast_tv);
-        //设置显示的内容
-        title.setText(message);
-        Toast toast = new Toast(context);
-        //设置Toast要显示的位置，水平居中并在底部，X轴偏移0个单位，Y轴偏移70个单位，
-        toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 280);
-        //设置显示时间
-        toast.setDuration(Toast.LENGTH_SHORT);
-
-        toast.setView(view);
-        toast.show();
+        Message msg = new Message();
+        msg.obj = message;
+        handler.sendMessage(msg);
     }
+
+
+    private static Handler handler = new Handler(AppManager.getAppManager().curremtActivity().getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            String message = (String) msg.obj;
+            Context context = AppManager.getAppManager().curremtActivity();
+            //使用布局加载器，将编写的toast_layout布局加载进来
+            View view = LayoutInflater.from(context).inflate(R.layout.toast_view, null);
+            //获取TextView
+            TextView title = view.findViewById(R.id.toast_tv);
+            //设置显示的内容
+            title.setText(message);
+            Toast toast = new Toast(context);
+            //设置Toast要显示的位置，水平居中并在底部，X轴偏移0个单位，Y轴偏移70个单位，
+            toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 280);
+            //设置显示时间
+            toast.setDuration(Toast.LENGTH_SHORT);
+
+            toast.setView(view);
+            toast.show();
+        }
+    };
 
 }
